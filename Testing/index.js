9d002c506e529953ad6ad2ddeb410e9363bc4e9d
Vladimir Gjurov <vladimir.gjurov@outlook.com>
2023-04-22 14:55:31 +1000

    Learning about random functions

--- Testing/index.js
+++ Testing/index.js
@@ -1,6 +1,8 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard] // array - ordered list of items
+// 2. Use getRandomCard() to set the values of firstCard and secondCard
+
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -9,14 +11,22 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+// 1. Create a function, getRandomCard(), that always returns the number 5
+function getRandomCard() {
+    return 5
+}
+
+
 function startGame() {
     renderGame()
 }
 
 function renderGame() {
-    // render out firstCard and secondCard
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
-    // render out ALL the cards we have
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -32,7 +42,11 @@ function renderGame() {
 
 
 function newCard() {
-    let card = 6
+    // 3. Use the getRandomCard() to set the value of card
+    let card = getRandomCard()
+    let secondCard = 4
     sum += card
+    cards.push(card)
+    console.log(cards)
     renderGame()
 }
