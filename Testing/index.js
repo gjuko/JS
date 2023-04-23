522c6e312d5fc257c07f59f177d00ff54b59a9fd
Vladimir Gjurov <vladimir.gjurov@outlook.com>
2023-04-23 18:56:14 +1000

    Final

--- Testing/index.js
+++ Testing/index.js
@@ -1,23 +1,37 @@
-// 2. Use getRandomCard() to set the values of firstCard and secondCard
+let player = {
+    name: "Per",
+    chips: 200
+}
 
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
 
-// 1. Create a function, getRandomCard(), that always returns the number 5
+playerEl.textContent = player.name + ": $" + player.chips
+
 function getRandomCard() {
-    return 5
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
 }
 
-
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
@@ -42,11 +56,10 @@ function renderGame() {
 
 
 function newCard() {
-    // 3. Use the getRandomCard() to set the value of card
-    let card = getRandomCard()
-    let secondCard = 4
-    sum += card
-    cards.push(card)
-    console.log(cards)
-    renderGame()
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
 }
