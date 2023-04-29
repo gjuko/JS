cdbf9bba839288d3612ca723bf3e7b5236ea6dbb
Vladimir Gjurov <vladimir.gjurov@outlook.com>
2023-04-29 14:53:05 +1000

    Almost To-do app

---
+++ Testing/Loops/index.js
@@ -0,0 +1,45 @@
+// let hands = ["rock", "paper", "scissor"]
+
+// // Create a function that returns a random item from the array
+
+// function getHand() {
+//     let randomIndex = Math.floor( Math.random() * 3)
+//     return hands[randomIndex]
+// }
+
+// console.log( getHand() )
+
+// let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+// let stageEl = document.getElementById("stage")
+// let fightButton = document.getElementById("fightButton")
+
+// fightButton.addEventListener("click", function() {
+//     // Challenge:
+//     // When the user clicks on the "Pick Fighters" button, pick two random 
+//     // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+//     let randomIndexOne = Math.floor( Math.random() * fighters.length )
+//     let randomIndexTwo = Math.floor( Math.random() * fighters.length )
+//     stageEl.textContent = fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo]
+// })
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+// let appleShelf = document.getElementById("apple-shelf")
+// let orangeShelf = document.getElementById("orange-shelf")
+
+// Create a function that puts the apples onto the appleShelf
+// and the oranges onto the orangeShelf. Use a for loop,
+// a conditional statement, and the textContent property.
+
+// function sortFruit() {
+//     for (let i = 0; i < fruit.length; i++) {
+//         if (fruit[i] === "🍎") {
+//             appleShelf.textContent += "🍎"
+//         } else if (fruit[i] === "🍊") {
+//             orangeShelf.textContent += "🍊"
+//         }
+//     }
+// }
+
+// sortFruit()
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
