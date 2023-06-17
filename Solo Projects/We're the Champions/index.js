afc6b0c4ff2512b6ab85ec82a11c5a35c6a6ad28
Vladimir Gjurov <vladimir.gjurov@outlook.com>
2023-06-17 19:37:04 +1000

    Managed to make a post and push to the other fiedlds below and blank out the top one

--- Solo Projects/We're the Champions/index.js
+++ Solo Projects/We're the Champions/index.js
@@ -12,9 +12,49 @@ const endorsementsInDB = ref(database, "Endorsements")
 const publishBtn = document.getElementById("publish-btn")
 const inputFieldEl = document.getElementById("main-input")
 const endorsementsFieldOne = document.getElementById("endfield1")
+const endorsementsFieldTwo = document.getElementById("endfield2")
+const endorsementsFieldThree = document.getElementById("endfield3")
+const fromFieldBtn = document.getElementById("From-field")
+const toFieldBtn = document.getElementById("To-field")
+
+
+
+
+
+
+
+let previousValue = ""
+let tempValue = ""
 
 publishBtn.addEventListener("click", function() {
+
+    let ToValue = toFieldBtn.value
+    toFieldBtn.innerHTML = ToValue;
+
+
     let inputValue = inputFieldEl.value
-    push(endorsementsInDB, inputValue)
-    endorsementsFieldOne.innerHTML = inputValue
+    // push(endorsementsInDB, inputValue)
+    
+    if (previousValue !== "") {
+        endorsementsFieldOne.innerHTML = previousValue;
+    }
+    
+    if (tempValue !== "") {
+        endorsementsFieldThree.innerHTML = tempValue;
+    }
+    
+    endorsementsFieldTwo.innerHTML = endorsementsFieldOne.innerHTML;
+    endorsementsFieldOne.innerHTML = inputValue;
+    tempValue = endorsementsFieldTwo.innerHTML;
+    previousValue = inputValue;
+
+    clearInputFieldEl()
 })
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function endrsmntOne() {
+    endrsmntOneValue.value = ""
+}
