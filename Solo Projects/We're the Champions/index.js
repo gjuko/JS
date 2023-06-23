5a5b2447b2ea05d198c5c1b021dfeb4746fac0c8
Vladimir Gjurov <vladimir.gjurov@outlook.com>
2023-06-23 19:28:45 +1000

    Making serious progress

--- Solo Projects/We're the Champions/index.js
+++ Solo Projects/We're the Champions/index.js
@@ -1,93 +1,70 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-    databaseURL: "https://playground-7c965-default-rtdb.asia-southeast1.firebasedatabase.app/"
-}
+  databaseURL: "https://playground-7c965-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const endorsementsInDB = ref(database, "Endorsements")
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const endorsementsInDB = ref(database, "Endorsements");
 
-const publishBtn = document.getElementById("publish-btn")
-const inputFieldEl = document.getElementById("main-input")
-const endorsementsFieldOne = document.getElementById("endfield1")
-const endorsementsFieldTwo = document.getElementById("endfield2")
-const endorsementsFieldThree = document.getElementById("endfield3")
-const fromFieldBtn = document.getElementById("From-field")
-const toFieldBtn = document.getElementById("To-field")
+const publishBtn = document.getElementById("publish-btn");
+const inputFieldEl = document.getElementById("main-input");
+const endorsementsFieldOne = document.getElementById("endfield1");
+const endorsementsFieldTwo = document.getElementById("endfield2");
+const endorsementsFieldThree = document.getElementById("endfield3");
+const fromFieldEl = document.getElementById("From-field");
+const toFieldEl = document.getElementById("To-field");
+const endorsFrom1 = document.getElementById("endorsementFrom1");
+const endorsFrom2 = document.getElementById("endorsementFrom2");
+const endorsFrom3 = document.getElementById("endorsementFrom3");
+const endorsTo1 = document.getElementById("endorsementTo1");
+const endorsTo2 = document.getElementById("endorsementTo2");
+const endorsTo3 = document.getElementById("endorsementTo3");
 
-
-let previousValue = ""
-let tempValue = ""
-
-let tempFromValue = ""
-let tempToValue = ""
+let previousValue = "";
+let tempValue = "";
+let previousfromValue = "";
+let tempfromValue = "";
+let previousToValue = "";
+let tempToValue = "";
 
 
 publishBtn.addEventListener("click", function() {
-
-    let fromValue = fromFieldBtn.value;
-    let toValue = toFieldBtn.value;
-
-    tempFromValue = fromValue;
-    tempToValue = toValue;
-    
-    let inputValue = inputFieldEl.value
+    let fromValue = fromFieldEl.value;
+    let inputValue = inputFieldEl.value;
+    let toValue = toFieldEl.value;
     // push(endorsementsInDB, inputValue)
-    
-    if (previousValue !== "") {
-        endorsementsFieldOne.innerHTML = previousValue;
-    }
-    
-    if (tempValue !== "") {
-        endorsementsFieldThree.innerHTML = tempValue;
-    }
-    
+    endorsementsFieldThree.innerHTML = endorsementsFieldTwo.innerHTML;
     endorsementsFieldTwo.innerHTML = endorsementsFieldOne.innerHTML;
     endorsementsFieldOne.innerHTML = inputValue;
     tempValue = endorsementsFieldTwo.innerHTML;
     previousValue = inputValue;
 
-    
-    
+    endorsFrom3.innerHTML = endorsFrom2.innerHTML;
+    endorsFrom2.innerHTML = endorsFrom1.innerHTML;
+    endorsFrom1.innerHTML = fromValue;
+    tempfromValue = endorsFrom2.innerHTML;
+    previousfromValue = fromValue;
 
+    endorsTo3.innerHTML = endorsTo2.innerHTML;
+    endorsTo2.innerHTML = endorsTo1.innerHTML;
+    endorsTo1.innerHTML = toValue;
+    tempToValue = endorsTo2.innerHTML;
+    previousToValue = toValue;
 
-    // Update "To" and "From" values in the labels
-    let Endorsement1FromField = document.querySelector('.endorsementTo1').innerHTML = "To: " + toValue;
-    let Endorsement1ToField = document.querySelector('.endorsementFrom1').innerHTML = "From: " + fromValue;
-
-    publishBtn.addEventListener("click", function() {
-        // Get the values from the "From" and "To" buttons of endorsements-container1
-        let endorsementFrom1 = document.querySelector('.endorsementFrom1').innerHTML;
-        let endorsementTo1 = document.querySelector('.endorsementTo1').innerHTML;
-    
-        // Move the values to endorsements-container2
-        document.querySelector('.endorsementFrom2').innerHTML = endorsementFrom1;
-        document.querySelector('.endorsementTo2').innerHTML = endorsementTo1;
-
-        document.querySelector('.endorsementFrom3').innerHTML = endorsementFrom2;
-        document.querySelector('.endorsementTo3').innerHTML = endorsementTo2;
-    
-    
-    });
-    
-
-
-    clearInputFieldEl()
-})
+  clearInputFieldEl();
+});
 
 function clearInputFieldEl() {
-    inputFieldEl.value = ""
+  inputFieldEl.value = "";
+  fromFieldEl.value = "";
+  toFieldEl.value = "";
 }
 
-function endrsmntOne() {
-    endrsmntOneValue.value = ""
-}
 
 onValue(endorsementsInDB, function (snapshot) {
-    endorsementBoxes.innerHTML = "";
-  
     if (snapshot.exists()) {
       const allEndorsement = Object.entries(snapshot.val());
       allEndorsement.forEach((el) => {
@@ -96,3 +73,34 @@ onValue(endorsementsInDB, function (snapshot) {
     }
 });
 
+
+
+
+
+    // Variables
+
+const likeBtn = document.querySelector('.heart-icon');
+const numberOfLikesElement = document.querySelector('.number-of-likes');
+
+let numberOfLikes = Number.parseInt(numberOfLikesElement.textContent, 10);
+let isLiked = false;
+
+// Functions
+
+const likeClick = () => {
+  if (!isLiked) {
+    likeBtn.classList.add('isLiked');
+    numberOfLikes++;
+    numberOfLikesElement.textContent = numberOfLikes;
+    isLiked = !isLiked;
+  } else {
+    likeBtn.classList.remove('isLiked');
+    numberOfLikes--;
+    numberOfLikesElement.textContent = numberOfLikes;
+    isLiked = !isLiked;
+  }
+};
+
+// Event Listeners
+
+likeBtn.addEventListener('click', likeClick);
