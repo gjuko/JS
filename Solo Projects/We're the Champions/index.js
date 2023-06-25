eb74167c16ee89fbf8681d4a6d99aaf676d7db3e
Vladimir Gjurov <vladimir.gjurov@outlook.com>
2023-06-25 19:46:55 +1000

    Finally done

--- Solo Projects/We're the Champions/index.js
+++ Solo Projects/We're the Champions/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getDatabase, ref, push, onValue, update } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
   databaseURL: "https://playground-7c965-default-rtdb.asia-southeast1.firebasedatabase.app/"
@@ -41,6 +41,11 @@ likeButtons.forEach(button => {
     }
 
     likeCountEl.textContent = currentLikes;
+
+    // Update the like count in the database
+    let endorsementKey = this.parentNode.parentNode.id;
+    let endorsementRef = ref(database, `Endorsements/${endorsementKey}`);
+    update(endorsementRef, { likeCountEl: currentLikes });
   });
 });
 
@@ -51,30 +56,57 @@ let tempfromValue = "";
 let previousToValue = "";
 let tempToValue = "";
 
-
 publishBtn.addEventListener("click", function() {
-    let fromValue = fromFieldEl.value;
-    let inputValue = inputFieldEl.value;
-    let toValue = toFieldEl.value;
-    
-    push(endorsementsInDB, inputValue)
-    endorsementsFieldThree.innerHTML = endorsementsFieldTwo.innerHTML;
-    endorsementsFieldTwo.innerHTML = endorsementsFieldOne.innerHTML;
-    endorsementsFieldOne.innerHTML = inputValue;
-    tempValue = endorsementsFieldTwo.innerHTML;
-    previousValue = inputValue;
+  let fromValue = fromFieldEl.value;
+  let inputValue = inputFieldEl.value;
+  let toValue = toFieldEl.value;
 
-    endorsFrom3.innerHTML = endorsFrom2.innerHTML;
-    endorsFrom2.innerHTML = endorsFrom1.innerHTML;
-    endorsFrom1.innerHTML = fromValue;
-    tempfromValue = endorsFrom2.innerHTML;
-    previousfromValue = fromValue;
+  const endorsementObj = {
+    content: inputValue,
+    from: fromValue,
+    to: toValue,
+    likeCountEl: 0
+  };
 
-    endorsTo3.innerHTML = endorsTo2.innerHTML;
-    endorsTo2.innerHTML = endorsTo1.innerHTML;
-    endorsTo1.innerHTML = toValue;
-    tempToValue = endorsTo2.innerHTML;
-    previousToValue = toValue;
+  const newEndorsementRef = push(endorsementsInDB);
+  const newEndorsementKey = newEndorsementRef.key;
+
+  const updates = {};
+  updates[newEndorsementKey] = endorsementObj;
+
+  update(endorsementsInDB, updates)
+    .then(() => {
+      clearInputFieldEl();
+    })
+    .catch((error) => {
+      console.error("Error pushing data to Firebase:", error);
+    });
+
+  endorsementsFieldThree.innerHTML = endorsementsFieldTwo.innerHTML;
+  endorsementsFieldTwo.innerHTML = endorsementsFieldOne.innerHTML;
+  endorsementsFieldOne.innerHTML = inputValue;
+  tempValue = endorsementsFieldTwo.innerHTML;
+  previousValue = inputValue;
+
+  endorsFrom3.innerHTML = endorsFrom2.innerHTML;
+  endorsFrom2.innerHTML = endorsFrom1.innerHTML;
+  endorsFrom1.innerHTML = fromValue;
+  tempfromValue = endorsFrom2.innerHTML;
+  previousfromValue = fromValue;
+
+  endorsTo3.innerHTML = endorsTo2.innerHTML;
+  endorsTo2.innerHTML = endorsTo1.innerHTML;
+  endorsTo1.innerHTML = toValue;
+  tempToValue = endorsTo2.innerHTML;
+
+  const likeCountEl1 = document.getElementById("likeCount1");
+  const likeCountEl2 = document.getElementById("likeCount2");
+  const likeButton1 = document.getElementById("likeButton1");
+  const likeButton2 = document.getElementById("likeButton2");
+
+  likeCountEl2.textContent = likeCountEl1.textContent;
+  likeButton2.dataset.liked = likeButton1.dataset.liked;
+  likeButton2.textContent = likeButton1.textContent;
 
   clearInputFieldEl();
 });
@@ -85,15 +117,48 @@ function clearInputFieldEl() {
   toFieldEl.value = "";
 }
 
-onValue(endorsementsInDB, function (snapshot) {
+function fetchExistingValues() {
+  onValue(endorsementsInDB, function (snapshot) {
     if (snapshot.exists()) {
-      let allEndorsements = Object.entries(snapshot.val());
-      
-      for (let i = 0; i < allEndorsements.length; i++) {
-        let currentItem = allEndorsements[i]
-    }   
-      allEndorsements.forEach((el) => {
-        renderText(el);
+      const endorsements = snapshot.val();
+      endorsementsFieldOne.innerHTML = "";
+      endorsementsFieldTwo.innerHTML = "";
+      endorsementsFieldThree.innerHTML = "";
+      endorsFrom1.innerHTML = "";
+      endorsFrom2.innerHTML = "";
+      endorsFrom3.innerHTML = "";
+      endorsTo1.innerHTML = "";
+      endorsTo2.innerHTML = "";
+      endorsTo3.innerHTML = "";
+
+      Object.entries(endorsements).forEach(([key, value]) => {
+        addNewElement(value);
+
+        endorsementsFieldThree.innerHTML = endorsementsFieldTwo.innerHTML;
+        endorsementsFieldTwo.innerHTML = endorsementsFieldOne.innerHTML;
+        endorsementsFieldOne.innerHTML = value.content;
+
+        endorsFrom3.innerHTML = endorsFrom2.innerHTML;
+        endorsFrom2.innerHTML = endorsFrom1.innerHTML;
+        endorsFrom1.innerHTML = value.from;
+
+        endorsTo3.innerHTML = endorsTo2.innerHTML;
+        endorsTo2.innerHTML = endorsTo1.innerHTML;
+        endorsTo1.innerHTML = value.to;
       });
     }
+  });
+}
+
+window.addEventListener('load', function () {
+  fetchExistingValues();
 });
+
+database.ref('.info/connected').on('value', function (snapshot) {
+  if (snapshot.val() === true) {
+    fetchExistingValues();
+  }
+});
+
+function addNewElement(value) {
+}
