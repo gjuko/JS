c7d91a660e41ce64ec8f4e8e615779131d745c38
Vladimir Gjurov <vladimir.gjurov@outlook.com>
2023-07-13 20:10:32 +1000

    Making progress

--- Solo Projects/Instagram/index.js
+++ Solo Projects/Instagram/index.js
@@ -1,54 +1,101 @@
 const posts = [
     {
-        name: "BMW S1000 RR M",
+        name: "BMW-MOTTORAD",
         username: "gjuko",
         location: "Munich, Germany",
-        avatar: "/Solo Projects/Instagram/images/bmw-logo.png",
-        post: "/Solo Projects/Instagram/images/bmws1000rrm.jpg",
+        avatar: "images/bmw-logo.png",
+        post: "images/bmws1000rrm.jpg",
         comment: "Just look at this beauty",
         likes: 998
     },
     {
-        name: "Ducati Panigale V4",
+        name: "Ducati",
         username: "every.biker",
-        location: "Ornans, France",
-        avatar: "/Solo Projects/Instagram/images/ducati-logo.jpeg",
-        post: "/Solo Projects/Instagram/images/ducatipanigalev4.jpeg",
+        location: "Italy",
+        avatar: "images/ducati-logo.jpeg",
+        post: "images/ducatipanigalev4.jpeg",
         comment: "what a stunning machine",
         likes: 760
     },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "/Solo Projects/Instagram/images/Kawasaki-Logo.jpg",
-        post: "/Solo Projects/Instagram/images/kawasakininjah2r.jpeg",
+    {
+        name: "Kawasaki",
+        username: "Ninja",
+        location: "Japan",
+        avatar: "images/Kawasaki-Logo.jpg",
+        post: "images/kawasakininjah2r.jpeg",
         comment: "This is a real missile",
         likes: 700
     }
-]
+];
 
-// Preload like images
-const heartRegular = new Image();
-heartRegular.src = "images/heart-regular.svg";
-const heartHover = new Image();
-heartHover.src = "images/heart-hover.svg";
+const photos = [
+    "images/like.svg",
+    "images/comment.svg",
+    "images/share.svg",
+];
+function createPostElements() {
+    const container = document.getElementById("post-container");
+    // container.innerHTML = ""; // Clear the container before adding new posts
 
-const postSection = document.getElementById('post-section')
+    posts.forEach((post, index) => {
+        const postContainer = document.createElement("div");
+        postContainer.classList.add("post-container");
 
-// Build each post in posts array
-for (let i = 0; i < posts.length; i++) {
-    // create elements
-    const postDiv = document.createElement('div');
-    const userInfoDiv = document.createElement('div');
-    const avatarImg = document.createElement('img');
-    const postNameP = document.createElement('p');
-    const postImg = document.createElement('img');
-    const postIconsDiv = document.createElement('div');
-    const likeImg = document.createElement('img');
-    const commentImg = document.createElement('img');
-    const shareImg = document.createElement('img');
-    const postLikesP = document.createElement('p');
-    const postCaptionP = document.createElement('p');
+        const postDiv = document.createElement("div");
+        postDiv.classList.add("post");
 
+        const postInfo = document.createElement("div"); // Create a container for postAvatar, name, and location
+        postInfo.classList.add("post-info");
+
+        const postAvatar = document.createElement("img");
+        postAvatar.classList.add("posts-avatar");
+        postAvatar.src = post.avatar;
+        postInfo.appendChild(postAvatar);
+
+        const postHeading = document.createElement("h2");
+        postHeading.classList.add("name");
+        postHeading.textContent = post.name;
+        postInfo.appendChild(postHeading);
+
+        postDiv.appendChild(postInfo);
+
+        const postLocation = document.createElement("h2");
+        postLocation.classList.add("location");
+        postLocation.textContent = post.location;
+        postInfo.appendChild(postLocation);
+
+        const postImage = document.createElement("img");
+        postImage.classList.add("post-image");
+        postImage.src = post.post;
+        postDiv.appendChild(postImage);
+
+        const postLikes = document.createElement("p");
+        postLikes.classList.add("likes");
+        postLikes.textContent = post.likes + " likes";
+        postDiv.appendChild(postLikes);
+
+        const postUsername = document.createElement("p");
+        postUsername.classList.add("username");
+        postUsername.textContent = post.username;
+        postDiv.appendChild(postUsername);
+
+        const postComment = document.createElement("p");
+        postComment.classList.add("comment");
+        postComment.textContent = post.comment;
+        postDiv.appendChild(postComment);
+
+        photos.forEach(photo => {
+            const photoImg = document.createElement("img");
+            photoImg.classList.add("icons");
+            photoImg.src = photo;
+            postDiv.insertBefore(photoImg, postLikes);
+        });
+
+        postContainer.appendChild(postDiv);
+        container.appendChild(postContainer);
+    });
 }
+
+
+// Call the function when the page loads
+window.addEventListener("load", createPostElements);
