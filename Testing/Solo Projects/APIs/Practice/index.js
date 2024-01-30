cb33f78e3d25a87e22bc35f965ae4b8835ecdec6
Vladimir Gjurov <vladimir.gjurov@outlook.com>
2024-01-30 21:03:01 +1100

    Progress

--- Testing/Solo Projects/APIs/Practice/index.js
+++ Testing/Solo Projects/APIs/Practice/index.js
@@ -1,54 +1,45 @@
-let postsArray = []
-const titleInput = document.getElementById("post-title")
-const bodyInput = document.getElementById("post-body")
-const form = document.getElementById("new-post")
 
-function renderPosts() {
-    let html = ""
-    for (let post of postsArray) {
-        html += `
-            <h3>${post.title}</h3>
-            <p>${post.body}</p>
-            <hr />
-        `
-    }
-    document.getElementById("blog-list").innerHTML = html
-}
+// let postsArray = []
+// const coloursForm = document.getElementById("new-post")
 
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-    .then(res => res.json())
-    .then(data => {
-        postsArray = data.slice(0, 5)
-        renderPosts()
-    })
+// const options = {
+//     method: "GET",
+//     body: JSON.stringify(data),
+//     headers: {
+//         "Content-Type": "application/json"
+//     }
+// }
 
-form.addEventListener("submit", function(e) {
-    e.preventDefault()
-    const postTitle = titleInput.value
-    const postBody = bodyInput.value
-    const data = {
-        title: postTitle,
-        body: postBody
-    }
-    
-    const options = {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }
-    
-    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
-        .then(res => res.json())
-        .then(post => {
-            postsArray.unshift(post)
-            renderPosts()
-            /**
-             * Challenge: clear the form out!
-             */
-            titleInput.value = ""
-            bodyInput.value = ""
-            // form.reset()
-        })
-})
+fetch("https://www.thecolorapi.com/id?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=html")
+  .then(res => res.text())  // Use res.text() instead of res.json()
+  .then(data => {
+    console.log(data);
+    document.getElementById("colors").innerHTML = data;  // Use innerHTML instead of textContent
+  });
+
+
+
+
+// let postsArray = []
+// const titleInput = document.getElementById("post-title")
+// const bodyInput = document.getElementById("post-body")
+// const coloursForm = document.getElementById("new-post")
+
+// function renderPosts() {
+//     let html = ""
+//     for (let post of postsArray) {
+//         html += `
+//             <h3>${post.title}</h3>
+//             <p>${post.body}</p>
+//             <hr />
+//         `
+//     }
+//     document.getElementById("blog-list").innerHTML = html
+// }
+
+// fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+//     .then(res => res.json())
+//     .then(data => {
+//         postsArray = data.slice(0, 5)
+//         renderPosts()
+//     })
